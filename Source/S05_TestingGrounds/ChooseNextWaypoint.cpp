19fc7e220b5f90bce02730aa3d40cd99e7355db3
George Rooney <grooney4@gmail.com>
2016-09-27 21:28:50 -0400

    Converted AI Character's movements from Blueprint to C++

--- Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/S05_TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,12 +2,29 @@
 
 #include "S05_TestingGrounds.h"
 #include "ChooseNextWaypoint.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" //TODO remove coupling
 #include "BehaviorTree/BlackboardComponent.h"
 
+
 EBTNodeResult::Type  UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
-	auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	//TODO protect against empty patrol points
+	//Get the patrol points
+	auto AIController = OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
+	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+
+	//Set next waypoint
+    auto BlackBoardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackBoardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint Index: %i"), Index);
+	BlackBoardComp->SetValueAsObject(Waypointkey.SelectedKeyName, PatrolPoints[Index]);
+
+	 //cycle the Index
+
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackBoardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+	
 	return EBTNodeResult::Succeeded;
 }
 
